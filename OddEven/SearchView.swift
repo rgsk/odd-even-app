6b88858acf14a946a2a5b122534be0f863983c0c
rgsk <guptarahul70322@gmail.com>
2023-07-15 23:50:16 +0530

    search view animation

--- OddEven/SearchView.swift
+++ OddEven/SearchView.swift
@@ -8,13 +8,112 @@
 import SwiftUI
 
 struct SearchView: View {
+    @StateObject private var viewModel = PlayerViewModel()
+    @State var searchQuery: String = ""
+    @State var selectedPlayers: [String]
+    @State var selectedPlayersInCurrentSession: [String] = []
+    let onCancel: () -> ()
+    let onDone: (_ newSelectedPlayers:[String]) -> ()
+    @FocusState private var isTextFieldFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                HStack {
+                    Button(action: {
+                        onCancel()
+                    }) {
+                        Text("Cancel")
+                    }
+                    Spacer()
+                    Button(action: {
+                        onDone(selectedPlayers)
+                    }) {
+                        Text("Done")
+                    }
+                }.padding()
+                
+                TextField("Search", text: $searchQuery)
+                    .padding(.horizontal)
+                    .onChange(of: searchQuery) { query in
+                        viewModel.filterPlayers(with: query)
+                    }
+                    .focused($isTextFieldFocused)
+                    .onAppear {
+                        isTextFieldFocused = true
+                    }
+            }
+            
+            if !selectedPlayersInCurrentSession.isEmpty {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 8) {
+                        ForEach(selectedPlayersInCurrentSession, id: \.self) { player in
+                            SelectedItemPillView(player: player, onCrossClick: {
+                                removePlayer(player)
+                            })
+                            
+                        }
+                    }
+                    .padding()
+                }
+            }
+            
+            
+            Divider()
+            
+            List(viewModel.displayedPlayers, id: \.self) { player in
+                let notShow = selectedPlayers.contains(player) && !selectedPlayersInCurrentSession.contains(player)
+                if !notShow {
+                    Button(action: {
+                        handlePlayerClick(player)
+                    }) {
+                        HStack {
+                            Text(player)
+                            Spacer()
+                            if selectedPlayersInCurrentSession.contains(player) {
+                                Image(systemName: "checkmark")
+                                    .foregroundColor(.accentColor)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func handlePlayerClick(_ player: String) {
+        if !selectedPlayers.contains(player) {
+            addPlayer(player)
+        } else {
+            removePlayer(player)
+        }
+    }
+    
+    private func removePlayer(_ player: String) {
+        selectedPlayers.removeAll { $0 == player }
+        selectedPlayersInCurrentSession.removeAll { $0 == player }
+    }
+    
+    private func addPlayer(_ player: String) {
+        selectedPlayers.append(player)
+        selectedPlayersInCurrentSession.append(player);
+        
+    }
+}
+
+
+
+struct SearchViewWrapperView: View {
+    var body: some View {
+        SearchView(selectedPlayers: ["Suresh Raina"], onCancel: {
+        }, onDone: { newSelectedPlayers in
+            
+        })
     }
 }
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchView()
+        SearchViewWrapperView()
     }
 }
--- OddEven/SelectTeamView.swift
+++ OddEven/SelectTeamView.swift
@@ -15,31 +15,41 @@ struct SelectTeamView: View {
     
     var body: some View {
         VStack {
-            TextField("Search", text: $searchQuery)
-                .padding()
-                .onChange(of: searchQuery) { query in
-                    viewModel.filterPlayers(with: query)
-                }
-                .onTapGesture {
-                    isSearchFocused = true
-                }
-            
-            Divider()
-            
-            VStack {
-                Text("Selected Players:")
-                    .font(.headline)
-                
-                List {
-                    ForEach(selectedPlayers, id: \.self) { player in
-                        Text(player)
+            if isSearchFocused {
+                SearchView(selectedPlayers: selectedPlayers, onCancel: {
+                    withAnimation{
+                        isSearchFocused = false
                     }
-                    .onMove(perform: movePlayer)
-                }
-            }.padding(.top,12)
-        }
-        .fullScreenCover(isPresented: $isSearchFocused) {
-            SearchView(searchQuery: $searchQuery, viewModel: viewModel, selectedPlayers: $selectedPlayers, isSearchFocused: $isSearchFocused)
+                }, onDone: { newSelectedPlayers in
+                    withAnimation{
+                        selectedPlayers = newSelectedPlayers
+                        isSearchFocused = false
+                    }
+                })
+                .transition(.opacity) // Apply fade-in and fade-out transition
+            } else {
+                TextField("Search", text: $searchQuery)
+                    .padding()
+                    .onTapGesture {
+                        withAnimation {
+                            isSearchFocused = true
+                        }
+                    }
+                Divider()
+                VStack {
+                    Text("Selected Players:")
+                        .font(.headline)
+                    
+                    List {
+                        ForEach(selectedPlayers, id: \.self) { player in
+                            Text(player)
+                        }
+                        .onMove(perform: movePlayer)
+                    }
+                }.padding(.top,12)
+                    .transition(.opacity) // Apply fade-in and fade-out transition
+            }
+            
         }
     }
     
@@ -53,93 +63,6 @@ struct SelectTeamView: View {
     }
 }
 
-
-struct SearchView: View {
-    @Binding var searchQuery: String
-    @ObservedObject var viewModel: PlayerViewModel
-    @Binding var selectedPlayers: [String]
-    @Binding var isSearchFocused: Bool
-    @State var selectedPlayersInCurrentSession: [String] = []
-    
-    var body: some View {
-        VStack {
-            VStack {
-                HStack {
-                    Button(action: {
-                        isSearchFocused = false
-                        selectedPlayers = selectedPlayers.filter {!selectedPlayersInCurrentSession.contains($0)}
-                    }) {
-                        Text("Cancel")
-                    }
-                    Spacer()
-                    Button(action: {
-                        isSearchFocused = false
-                    }) {
-                        Text("Done")
-                    }
-                }.padding()
-                
-                TextField("Search", text: $searchQuery)
-                    .padding(.horizontal)
-                
-            }
-            
-            if !selectedPlayersInCurrentSession.isEmpty {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 8) {
-                        ForEach(selectedPlayersInCurrentSession, id: \.self) { player in
-                            SelectedItemPillView(player: player, onCrossClick: {
-                                removePlayer(player)
-                            })
-                          
-                        }
-                    }
-                    .padding()
-                }
-            }
-            
-            
-            Divider()
-            
-            List(viewModel.displayedPlayers, id: \.self) { player in
-                let notShow = selectedPlayers.contains(player) && !selectedPlayersInCurrentSession.contains(player)
-                if !notShow {
-                    Button(action: {
-                        handlePlayerClick(player)
-                    }) {
-                        HStack {
-                            Text(player)
-                            Spacer()
-                            if selectedPlayersInCurrentSession.contains(player) {
-                                Image(systemName: "checkmark")
-                                    .foregroundColor(.accentColor)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    func handlePlayerClick(_ player: String) {
-        if !selectedPlayers.contains(player) {
-            addPlayer(player)
-        } else {
-            removePlayer(player)
-        }
-    }
-    
-    private func removePlayer(_ player: String) {
-        selectedPlayers.removeAll { $0 == player }
-        selectedPlayersInCurrentSession.removeAll { $0 == player }
-    }
-    
-    private func addPlayer(_ player: String) {
-        selectedPlayers.append(player)
-        selectedPlayersInCurrentSession.append(player);
-        
-    }
-}
 
 
 
